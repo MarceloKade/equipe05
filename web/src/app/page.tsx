73adcfa04ebafed0eda37c21fff968133f4c9930
MarceloKade <98852912+MarceloKade@users.noreply.github.com>
2024-08-09 05:52:58 -0300

    feat: Added component Feedbacks

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,16 +1,9 @@
-import HeroSection from "@/components/HeroSection/HeroSection";
-import NossaEquipe from "@/components/NossaEquipe/NossaEquipe";
-import Beneficios from "@/components/Beneficios/Beneficios";
 import Feedbacks from "@/components/Feedbacks/Feedbacks";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <HeroSection/>
-      <Beneficios/>
-      <NossaEquipe />
-      <Feedbacks/>
+      <Feedbacks />
     </>
   );
 }
